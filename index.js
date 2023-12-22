8fcf05be471fad909bd08bf9df202efaee03ffa0
Tzach Fleischer <tzahflisher@gmail.com>
2023-12-22 11:12:24 +0200

    added delete option and routing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-const blogList = [];
+var blogList = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -40,6 +40,27 @@ app.get("/blog",(req, res) => {
     //console.log();
     res.render("blog.ejs", blog);
 }); 
+
+app.get("/delete", (req, res) => {
+    res.render("delete.ejs");
+});
+
+app.post("/delete/submit", (req, res) => {
+    const nameToDelete = req.body["blogName"];
+    console.log(nameToDelete);
+    const blogToDelete = blogList.find(({blogName}) => blogName === nameToDelete);
+    var message = `${nameToDelete} was not found`;
+     
+    if (blogToDelete) {
+        blogList = blogList.filter(function(blog) { return blog.blogName != nameToDelete; });
+        message = `${nameToDelete} deleted succesfully`;
+    }
+    console.log(message);
+    console.log(blogList);
+    res.render("delete.ejs", {message : message});
+    
+
+});
 
 
 app.listen(port, () => {
