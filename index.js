bf705fd64abb023de7d8a28c963c72f3f78b804b
Tzach Fleischer <tzahflisher@gmail.com>
2023-12-22 11:18:40 +0200

    added documentation for routing

--- index.js
+++ index.js
@@ -10,44 +10,64 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+/*
+    displays main webpage with the list of blogs
+*/
 app.get("/", (req, res) => {
     res.render("index.ejs", {
         blogList : blogList,
     });
   });
 
+/*
+    displays addPost page for adding posts
+*/
 app.get("/addPost", (req,res) => {
 
     res.render("addBlog.ejs");
 });
 
+/*
+    deals with logic of adding posts
+    and then rerouts to main page with the updated list
+*/
 app.post("/submit", (req, res) => {
     const newBlog = {
         blogName : req.body["blogName"],
         blogBody : req.body["blogBody"],
     };
     blogList.push(newBlog);
-    //console.log(blogList);
 
     res.render("index.ejs", {
         blogList : blogList,
     });
 });
 
+
+/*
+    finds requested blog and displays it on the page
+*/
 app.get("/blog",(req, res) => {
     const nameToFind = req.query.blogName;
     const blog = blogList.find(({blogName}) => blogName === nameToFind);
-    //console.log();
-    res.render("blog.ejs", blog);
-}); 
 
+    res.render("blog.ejs", blog);
+});
+
+
+/*
+    displays delete blog page
+*/
 app.get("/delete", (req, res) => {
     res.render("delete.ejs");
 });
 
+
+/*
+    deals with the blog deletion logic, returns status message
+*/
 app.post("/delete/submit", (req, res) => {
     const nameToDelete = req.body["blogName"];
-    console.log(nameToDelete);
     const blogToDelete = blogList.find(({blogName}) => blogName === nameToDelete);
     var message = `${nameToDelete} was not found`;
      
@@ -55,11 +75,8 @@ app.post("/delete/submit", (req, res) => {
         blogList = blogList.filter(function(blog) { return blog.blogName != nameToDelete; });
         message = `${nameToDelete} deleted succesfully`;
     }
-    console.log(message);
-    console.log(blogList);
-    res.render("delete.ejs", {message : message});
-    
 
+    res.render("delete.ejs", {message : message});
 });
 
 
